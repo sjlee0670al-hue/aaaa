71fafd2977190857e08ebf56fc5e995099f3fddb
aaaa <sjlee0670al@gmail.com>
2025-11-17 10:24:55 +0900

    .....

--- day13/index.js
+++ day13/index.js
@@ -1,5 +1,38 @@
 export default async function main() {
 
-    console.log("hello js")
+    console.log('start app')
     
+
+
+let currentIndex = 1;
+const menuitems = document.querySelectorAll(".menu-item");
+
+console.log(menuitems)
+
+menuitems[currentIndex].classList.add("select")
+
+window.addEventListener("keydown", (e) => {
+    
+    console.log(e.key)
+
+    menuitems[currentIndex].classList.remove('select')
+
+    if(e.key == "ArrowDown") {
+        currentIndex--
+        if(currentIndex < 0) {
+            currentIndex = 2
+        }
+    }   
+    else if(e.key == "ArrowDown") {
+        currentIndex++
+        currentIndex %=3
+}
+    console.log(currentIndex)
+    menuitems[currentIndex].classList.add('select')
+
+})
+
+
+
+
 }
