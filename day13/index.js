0c47e3bd3b51d24b52e26db3b64b75222de34f9a
aaaa <sjlee0670al@gmail.com>
2025-11-24 10:24:42 +0900

    ......

--- day13/index.js
+++ day13/index.js
@@ -4,35 +4,40 @@ export default async function main() {
     
 
 
-let currentIndex = 1;
-const menuitems = document.querySelectorAll(".menu-item");
 
-console.log(menuitems)
+const menuitems = document.querySelectorAll(".menu-item");
+const creditScreen = document.querySelector("credit-screen")
+
+
+let currentIndex = 0;
+console.log(menuitems.length)
+const menuitems_count = menuitems.length
 
 menuitems[currentIndex].classList.add("select")
 
 window.addEventListener("keydown", (e) => {
-    
-    console.log(e.key)
+
 
     menuitems[currentIndex].classList.remove('select')
+})
+    console.log(e.key)
 
     if(e.key == "ArrowDown") {
-        currentIndex--
+        currentIndex--}
         if(currentIndex < 0) {
-            currentIndex = 2
-        }
+            currentIndex = menuitems_count
+        
     }   
     else if(e.key == "ArrowDown") {
         currentIndex++
-        currentIndex %=3
-}
+        currentIndex %=menuitems_count
+    }
+
+else if(e.key == "Enter") 
+    console.log(menuitems[currentIndex].dataset.action)
+    if(select_action == "credit")
+
+    
     console.log(currentIndex)
     menuitems[currentIndex].classList.add('select')
-
-})
-
-
-
-
 }
